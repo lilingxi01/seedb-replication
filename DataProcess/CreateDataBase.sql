b0bd8805f57f9bd82100c3588ef72a13af1309ff
Lucoin <148014505+Lucoin-yf@users.noreply.github.com>
2024-05-04 10:35:40 +0800

    Create CreateDataBase.sql

---
+++ DataProcess/CreateDataBase.sql
@@ -0,0 +1,35 @@
+CREATE TABLE adult_data (
+    age INT,
+    workclass VARCHAR(255),
+    fnlwgt INT,
+    education VARCHAR(255),
+    education_num INT,
+    marital_status VARCHAR(255),
+    occupation VARCHAR(255),
+    relationship VARCHAR(255),
+    race VARCHAR(255),
+    sex VARCHAR(255),
+    capital_gain INT,
+    capital_loss INT,
+    hours_per_week INT,
+    native_country VARCHAR(255),
+    income VARCHAR(255)
+);
+
+COPY adult_data FROM 'D:\course\645\project\pro_adult_data.data' WITH (FORMAT csv);
+
+select * from adult_data limit 10
+
+CREATE TABLE married_data AS 
+SELECT age, workclass, fnlwgt, education, education_num, occupation, relationship, race, sex, capital_gain, capital_loss, hours_per_week, native_country, income
+FROM adult_data
+WHERE marital_status = 'Married';
+
+select * from married_data limit 10
+
+CREATE TABLE unmarried_data AS 
+SELECT age, workclass, fnlwgt, education, education_num, occupation, relationship, race, sex, capital_gain, capital_loss, hours_per_week, native_country, income
+FROM adult_data
+WHERE marital_status = 'Unmarried';
+
+select * from unmarried_data limit 10
